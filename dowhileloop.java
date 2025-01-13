6cae5c9f8d9f95dbbbcffc65b553ac267acd679e
Jasleen Kaur <kaurjasleen6104@gmail.com>
2025-01-13 21:48:23 +0530

    Add files via upload

---
+++ dowhileloop.java
@@ -0,0 +1,9 @@
+public class dowhileloop {
+    public static void main(String[] args){
+        int i = 0;
+        do{
+            System.out.println("Hi World");
+            i++;
+        } while(i < 3);
+    }
+}
