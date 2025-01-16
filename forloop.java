ecd6d3b242762534539a0cd753a40e133cf9db51
Jasleen Kaur <kaurjasleen6104@gmail.com>
2025-01-16 22:33:49 +0530

    Add files via upload

---
+++ forloop.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class forloop {
+    public static void main(String[] args){
+        // for(int counter = 0; counter < 3; counter = counter + 1){
+        //     System.out.println("Hello World");
+        // }
+
+        // for(int i = 0; i < 11; i++){
+        //     System.out.println(i);
+        // }
+
+        // Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
+
+        // int sum = 0;
+
+        // for(int i = 1; i <= n; i++){
+        //     sum += i;
+        // }
+
+        // System.out.println(sum);
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= 10; i++){
+            System.out.println(i * n);
+        }
+    }
+}
